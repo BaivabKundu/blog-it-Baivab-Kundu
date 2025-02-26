adb4770c11a26b7a94fbddeaaa162b0540f64dce
Baivab Kundu <baivab.kundu@bigbinary.com>
2025-02-26 16:06:41 +0530

    Added reusable components

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import { Redirect } from "react-router-dom/cjs/react-router-dom.min";
+
+import BlogPosts from "components/BlogPosts";
+import Lists from "components/Lists";
 
 const App = () => (
   <Router>
     <Switch>
-      <Route exact path="/" render={() => <div>Home</div>} />
-      <Route exact path="/about" render={() => <div>About</div>} />
+      <Route exact component={BlogPosts} path="/blogs" />
+      <Route exact component={Lists} path="/lists" />
+      <Redirect path="*" to="/blogs" />
     </Switch>
   </Router>
 );
