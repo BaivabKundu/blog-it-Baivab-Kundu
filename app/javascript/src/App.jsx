97fa95799c00e0974b6f3afe63fb11fd12c806d6
Baivab Kundu <baivab.kundu@bigbinary.com>
2025-02-28 15:30:18 +0530

    Used neetoui Toastr component and added Axios interceptors

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import { Redirect } from "react-router-dom/cjs/react-router-dom.min";
+import { ToastContainer } from "react-toastify";
 
 import BlogPosts from "components/BlogPosts";
 import Lists from "components/Lists";
 
 const App = () => (
   <Router>
+    <ToastContainer />
     <Switch>
       <Route exact component={BlogPosts} path="/blogs" />
       <Route exact component={Lists} path="/lists" />
