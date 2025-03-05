cd115fe3a14b8b99de48d8a614da719e2eea4a8b
Baivab Kundu <baivab.kundu@bigbinary.com>
2025-03-05 12:28:33 +0530

    Created sidebar and added filtering

--- app/javascript/src/components/Sidebar/index.jsx
+++ app/javascript/src/components/Sidebar/index.jsx
@@ -1,36 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { Book, Edit, List } from "@bigbinary/neeto-icons";
-import { Avatar } from "@bigbinary/neetoui";
+import { Book, Category, Edit, List } from "@bigbinary/neeto-icons";
+import { Avatar, Button } from "@bigbinary/neetoui";
 import { NavLink } from "react-router-dom";
 
-const Sidebar = () => (
-  <div className="fixed flex h-screen w-16 flex-col items-center border-r border-gray-200 bg-white py-4">
-    <NavLink
-      activeClassName="bg-black text-white"
-      className="my-2 rounded-lg p-2 hover:bg-gray-400"
-      to="/blogs"
-    >
-      <Book className="h-6 w-6" />
-    </NavLink>
-    <NavLink
-      activeClassName="bg-black text-white"
-      className="my-2 rounded-lg p-2 hover:bg-gray-400"
-      to="/lists"
-    >
-      <List className="h-6 w-6" />
-    </NavLink>
-    <NavLink
-      activeClassName="bg-black text-white"
-      className="my-2 rounded-lg p-2 hover:bg-gray-400"
-      to="/posts/create"
-    >
-      <Edit className="h-7 w-7" />
-    </NavLink>
-    <div className="mb-6 mt-4 flex h-full flex-col justify-end">
-      <Avatar size="large" />
+import SidePane from "./SidePane";
+
+const Sidebar = ({ onCategorySearch, onCategorySelect, onSidebarOpen }) => {
+  const [isPaneOpen, setIsPaneOpen] = useState(false);
+
+  const handleSidePane = () => {
+    setIsPaneOpen(!isPaneOpen);
+    onSidebarOpen(!isPaneOpen);
+  };
+
+  return (
+    <div className="fixed flex h-screen">
+      <div className="flex w-16 flex-col items-center border-r border-gray-200 bg-white py-4">
+        <NavLink
+          activeClassName="bg-black text-white"
+          className="my-2 rounded-lg p-2 hover:bg-gray-400"
+          to="/blogs"
+        >
+          <Book className="h-6 w-6" />
+        </NavLink>
+        <NavLink
+          activeClassName="bg-black text-white"
+          className="my-2 rounded-lg p-2 hover:bg-gray-400"
+          to="/lists"
+        >
+          <List className="h-6 w-6" />
+        </NavLink>
+        <NavLink
+          activeClassName="bg-black text-white"
+          className="my-2 rounded-lg p-2 hover:bg-gray-400"
+          to="/posts/create"
+        >
+          <Edit className="h-7 w-7" />
+        </NavLink>
+        <Button
+          className="my-2 rounded-lg p-2 hover:bg-gray-400"
+          icon={() => <Category />}
+          label=""
+          style="text"
+          onClick={handleSidePane}
+        />
+        <div className="mb-6 mt-4 flex h-full flex-col justify-end">
+          <Avatar size="large" />
+        </div>
+      </div>
+      <SidePane
+        isOpen={isPaneOpen}
+        onCategorySearch={onCategorySearch}
+        onCategorySelect={onCategorySelect}
+      />
     </div>
-  </div>
-);
+  );
+};
 
 export default Sidebar;
