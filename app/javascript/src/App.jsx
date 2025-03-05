15320da5ae3c368ba13b977c89cb01c09f95904f
Baivab Kundu <baivab.kundu@bigbinary.com>
2025-03-05 12:31:01 +0530

    Restructured components

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,26 +1,12 @@
 import React from "react";
 
-import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
-import { Redirect } from "react-router-dom/cjs/react-router-dom.min";
-import { ToastContainer } from "react-toastify";
+import { BrowserRouter as Router } from "react-router-dom";
 
-import CreatePost from "components/CreatePost";
-import Lists from "components/Lists";
-import BlogPosts from "components/Posts";
-import ShowPost from "components/ShowPost";
-import Sidebar from "components/Sidebar";
+import Main from "components/Main";
 
 const App = () => (
   <Router>
-    <ToastContainer />
-    <Sidebar />
-    <Switch>
-      <Route exact component={ShowPost} path="/posts/:slug/show" />
-      <Route exact component={CreatePost} path="/posts/create" />
-      <Route exact component={BlogPosts} path="/blogs" />
-      <Route exact component={Lists} path="/lists" />
-      <Redirect path="*" to="/blogs" />
-    </Switch>
+    <Main />
   </Router>
 );
 
--- app/javascript/src/components/Lists/index.jsx
+++ app/javascript/src/components/Lists/index.jsx
@@ -2,18 +2,11 @@ import React from "react";
 
 import { Typography } from "@bigbinary/neetoui";
 
-import Sidebar from "components/Sidebar";
-
 const Lists = () => (
-  <div className="flex">
-    <Sidebar />
-    <main className="ml-24 flex-1">
-      <div className="w-full px-4 py-8">
-        <Typography className="mb-8 text-5xl font-bold text-gray-800">
-          Lists
-        </Typography>
-      </div>
-    </main>
+  <div className="ml-24 w-auto px-4 py-8">
+    <Typography className="mb-8 text-5xl font-bold text-gray-800">
+      Lists
+    </Typography>
   </div>
 );
 
